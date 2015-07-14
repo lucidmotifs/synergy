5c6161e03d036d8ba6e575cc0576073a29a8dc08
lucidmotifs <lucidmotifs@gmail.com>
2015-07-14 17:53:54 +1000

    Added very basic script for calculating damage over 10 seconds.
    This is the basis for creating various functions of synergy. From here
    we can start developing useful objects and methods.

--- proc_parse.py
+++ proc_parse.py
@@ -3,21 +3,53 @@
 # In future, hopefully this module can be used with just crit/pen glyph
 # points (plus minor crit / major crit chance)
 
-# Define proc damage. Should be related to Attack Power - TODO fine formula
+# Define proc damage. Should be related to Attack Power - TODO find formula
 class PROCS:
     ONE_IN_THE_CHAMBER = 264
     SUDDEN_RETURN = 264
     THUNDERSTRUCK = 394
 
+# This test is to see whether more damage is gained
+# from which elite passive.
+def test_compare_elite_passives():
+    ## Set All variables TODO make external variables
 
-def test_thunderstruck_vs_rapidgetaway():
     # test at different crit %
     crit_chance = 20
     # assume 1000dps (test scaling DPS later)
     dps = 1000
-    # This test is to see whether more damage is gained
-    # from which elite passive.
+    # laceration signet
+    have_laceration = True
+
 
     # combat in 10 seconds (approx single round)
     base_dmg = 10 * dps
+    base_crit = crit_chance  # percent
+    base_crit_dmg = .4
+
+    # with laceration signet
+    if have_laceration:
+        base_crit_dmg += .24
+
+    # assume no randomness over 10 hits
+    dmg_per_hit = dps
+
+    crits_per_round = base_crit / 10
+    crit_hit_dmg = dmg_per_hit * (1 + base_crit_dmg)
+    # builder dmg needs to be seperated from consumer (crit % will differ too...)
     crit_dmg = base_dmg * (crit_chance / 100)
+
+    crit_dmg_per_round = crit_hit_dmg * crits_per_round
+
+    proc_dmg_per_crit = 264 + 392
+
+    proc_dmg_per_round = proc_dmg_per_crit * crits_per_round
+
+    print("Base Damage: %d \n" % base_dmg)
+    print("Crit Damage: %d \n" % crit_dmg_per_round)
+    print("Proc Damage: %d \n" % proc_dmg_per_round)
+
+    return base_dmg + crit_dmg_per_round + proc_dmg_per_round
+
+
+print(test_compare_elite_passives())
