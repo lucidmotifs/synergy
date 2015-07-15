a56a5168b8cd386f0e3a86eb61967feacb9fcf68
lucidmotifs <lucidmotifs@gmail.com>
2015-07-15 18:21:55 +1000

    Crafting basic menu
    Idea: build menu items first - then use each menu item as an 'Epic' to
    create sub tasks for.

--- synergy.py
+++ synergy.py
@@ -17,6 +17,25 @@ def main(args, kwargs):
     # - create a build (modify an ability list)
     print("Welcome...S.Y.N.E.R.G.Y Online.")
 
+    _opt = ''
+    while not _opt == 'x' and not _opt == 'X':
+        # Display menu
+        print("Select from the options below:")
+        print("==============================")
+        print()
+        print("(B)uild an Avatar. Modify your player object for use in tests")
+        print("(R)un a Test. Select from a list of tests to run")
+        print("(C)reate a Build. Create/Modify an Ability list")
+        print()
+        print("E(x)it the program")
+        print()
+
+        _opt = input("Enter choice: ")
+
+        print("Your choice was {0}".format(_opt))
+
+        _opt = 'x'
+
     return 0
 
 # boilerplate python to run main() when script is launched from console
