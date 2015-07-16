d3aabd0e621812ced1a96e476a0d31bfc65e4297
lucidmotifs <lucidmotifs@gmail.com>
2015-07-16 14:28:27 +1000

    Added tests menu with example test
    Actual test still to be added

--- synergy.py
+++ synergy.py
@@ -4,6 +4,7 @@ import sys
 # synergy imports
 import abilities
 import affects
+import tests
 
 # Entry point for the application
 
@@ -34,9 +35,47 @@ def main(args, kwargs):
 
         print("Your choice was {0}".format(_opt))
 
+        if _opt == 'r' or _opt == 'R':
+            # Tests menu
+            display_tests_menu()
+
         _opt = 'x'
 
     return 0
+
+# Althoug the below function may suffice for the moment, the correct way to
+# implement console UI would be something like menus.<menu_name>.display()
+# i.e. menus.tests.display()
+# This way we can add options and have those options auto-run some
+# definition.
+def display_tests_menu():
+    _opt = ''
+    while not _opt == 'x' and not _opt == 'X':
+        # Display menu
+        print("Tests Menu:")
+        print("-----------")
+        print("Select from the options below:")
+        print("==============================")
+        print()
+        print("(1) Test Thunderstruck vs Rapid Getaway")
+        print()
+        print("E(x)it the menu")
+        print()
+
+        _opt = input("Enter choice:")
+
+        # capture the result of test
+        result = 0
+        if _opt == 1:
+            # Execute a test case
+            result = tests.thunderstruck_vs_rapidgetaway
+        else if _opt == 'x' or _opt == 'X':
+            print("Leaving menu...")
+            print()
+        else:
+            print("Invalid selection, please choose a valid menu item.")
+            print()
+
 
 # boilerplate python to run main() when script is launched from console
 if __name__ == "__main__":
