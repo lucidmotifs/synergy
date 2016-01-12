c62e2c5087e33c3431ac51e38187fb57672687cc
lucidmotifs <lucidmotifs@gmail.com>
2016-01-12 17:23:10 +1100

    Repaired broken test

--- func_tests/test_json_data_import.py
+++ func_tests/test_json_data_import.py
@@ -3,36 +3,35 @@
 import data
 
 def run():
-    # List of all abilities
-    test_data = data.load()
+	# List of all abilities
+	test_data = data.load()
 
-    # For each list in the test_data loaded.
-    for d in test_data:
-        # turn each result into a list
-        l = list(d.values())
+	# For each list in the test_data loaded.
+	for d in test_data.values():
+		# turn each result into a list
+		l = list(d.values())
 
-        print("Number of Abilities Loaded: %s" % len(l))
-        print()
-        print("Raw Data: \n")
-        print(d.values())
+		print("Number of Abilities Loaded: %s" % len(l))
+		print()
+		print("Raw Data: \n")
+		print(d.values())
 
-        print()
-        print("Printing random ability name: \n")
-        print(l[0]["name"])
-        print()
+		print()
+		print("Printing random ability name: \n")
+		print(l[0]["name"])
+		print()
 
-        print()
-        print("Display formatted provides tree: ")
-        print()
+		try:
+			provides = l[0]['provides']
+			for k,v in provides.items():
+				print("\nDisplay formatted provides tree: \n")
+				print(k)
+				print(v)
+				print()
+		except KeyError as e:
+			print("Fail: Ability selected does not have a provides attribute")
 
-        provides = l[0]['provides']
+	# line for readibility
+	print()
 
-        for k,v in provides.items():
-            print(k)
-            print(v)
-            print()
-
-    # line for readibility
-    print()
-
-    return 1  # pass
+	return 1  # pass
