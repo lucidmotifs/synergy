a7da62ec7d1969bc215a8409f4b44b1072767add
lucidmotifs <lucidmotifs@gmail.com>
2016-01-12 17:15:21 +1100

    Changed spaces to tabs

--- func_tests/test_create_ability_from_data.py
+++ func_tests/test_create_ability_from_data.py
@@ -8,31 +8,31 @@ from game.abilities import AbilityList
 # data.
 
 def run():
-    test_data = data.load()["actives"].values()  # active abilities
+	test_data = data.load()["actives"].values()  # active abilities
 
 	# fail test if test_data is empty
 	if test_data is None:
 		print ("Fail: No test data loaded")
 		return 0
 
-    al = AbilityList()
+	al = AbilityList()
 
-    # loop through data creating an ability from each 'name'
-    for ability in test_data:
-        #print(ability["name"])
-        if al.populate({'test_ability': ability}, "actives"):
-            print("\nAdded: %s" % ability["name"])
-            print()
-            print("Print object: \n")
-            try:
-                print(al.actives[ability["name"]])
-            except KeyError:
-                print("Invalid data, printing debug info...")
-                print(al.actives)
-            print()
-        else:
-            print("Failed!\n")
-            # TODO Add debug info as to why this test failed.
-            return 0
+	# loop through data creating an ability from each 'name'
+	for ability in test_data:
+		#print(ability["name"])
+		if al.populate({'test_ability': ability}, "actives"):
+			print("\nAdded: %s" % ability["name"])
+			print()
+			print("Print object: \n")
+			try:
+				print(al.actives[ability["name"]])
+			except KeyError:
+				print("Invalid data, printing debug info...")
+				print(al.actives)
+			print()
+		else:
+			print("Failed!\n")
+			# TODO Add debug info as to why this test failed.
+			return 0
 
-    return 1
+	return 1
